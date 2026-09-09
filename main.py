b7cbeb00f00694a02e564b537a726c89888cf1eb
Zachary-boop <royzach1234@gmail.com>
2026-09-09 16:28:30 -0400

    Remove beautifulSoup
    Removes beautifulsoup from the code

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import mistletoe
-from bs4 import BeautifulSoup
 import re
-from Code.file import build_tree
 from pathlib import Path
-
+from textwrap import dedent
 SHORTCUT = "!!"
-def build_tree(path, max_depth, current_depth=1):
+FILE = "testmark.md"
+
+def build_tree(path : str, max_depth, current_depth=1) -> dict | list:
+    """
+    Reads a folder and builds a tree consisting of all the files at a certain depth.
+    Can be used with Build_html
+
+    It returns a dict or list depending on the depth
+    """
+
     if path in [r'\\',r'\\'[0]]:
         path = r''
     path = Path(path)
     
-    if path.is_file():
+    if path.is_file(): ## Make sure the path is not just a file
         return path.name
         
-    if current_depth == max_depth:
-        # On filtre les éléments cachés à la profondeur maximale
+    if current_depth == max_depth: #Make sure not to go infinetly
+        
         return [f.name for f in path.iterdir() if not f.name.startswith('.')]
         
     tree = {}
@@ -23,121 +30,105 @@ def build_tree(path, max_depth, current_depth=1):
         if item.name.startswith('.'): #ignore folders with . before their name
             continue
             
-        if item.is_dir():
+        if item.is_dir(): # if the item is a folder go into it to continue the tree
             tree[item.name] = build_tree(item, max_depth, current_depth + 1)
         else:
             tree[item.name] = item.name
             
     return tree
 
-
 def build_html(tree: dict | list | str) -> str:
+    """
+    Builds the html tree
+    """
     html = ''
     
-    # 1. Cas d'une liste
+    # 1. if a list
     if isinstance(tree, list):
         for item in tree:
-            # On rappelle la fonction pour gérer si l'item est un dictionnaire, sous-liste ou str
+            
             html += f'<li><span class="file">{item}</span></li>' 
 
             
-    # 2. Cas d'une chaîne
+    # 2. if string
     elif isinstance(tree, str):
         html += f'<li><span class="file">{tree}</span></li>' 
 
-    # 3. Cas d'un dictionnaire 
+    # 3. if dict 
     elif isinstance(tree, dict):
         for folder_name, content in tree.items():
             if isinstance(content,list):
                 html += f'<li><span class="folder">{folder_name}</span><ul>'
-                html += build_html(content) # On passe le contenu (liste, dict, etc.)
+                html += build_html(content)
                 html += '</ul></li>'
             if isinstance(content,str):
                 html += f'<li><span class="file">{content}</span></li>' 
             if isinstance(content,dict):
                 html += f'<li><span class="folder">{folder_name}</span><ul>'
-                html += build_html(content) # On passe le contenu (liste, dict, etc.)
+                html += build_html(content)
                 html += '</ul></li>'
     return html
 
 def render_page(tree_dict):
-    # Appel de la fonction récursive corrigée au message précédent
+
     corps_arbre = build_html(tree_dict) 
     
-    return f"""<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8"/>
-    <link href="style.css" rel="stylesheet"/>
-</head>
-<body>
-    <div class="file-tree">
-        <ul>
-            {corps_arbre}
-        
-    </div>
-</body>
-</html>"""
+    raw_html =f"""<!DOCTYPE html>
+                <html>
+                <head>
+                    <meta charset="utf-8"/>
+                    <link href="style.css" rel="stylesheet"/>
+                </head>
+                <body>
+                    <div class="file-tree">
+                        <ul>
+                            {corps_arbre}
+                        
+                    </div>
+                </body>
+                </html>"""
+    return raw_html
 
-with open('testmark.md', 'r') as fin:
-    html_content = mistletoe.markdown(fin)
-# 2. Analyser le code avec BeautifulSoup
-soup = BeautifulSoup(html_content, "html.parser")
+with open(FILE ,"r") as fin:
+    CommonMark_content = fin.readlines()
 
-# 3. Créer la balise <head> principale
-head_tag = soup.new_tag("head")
+indexs = []
+for index, line in enumerate(CommonMark_content):
+    if line[0:2] == "!!":
+        print(line)
+        indexs.append(index)
 
-# 4. Créer les balises enfants à mettre dans le head
-meta_charset = soup.new_tag("meta", charset="UTF-8")
+print(indexs)
 
-title_tag = soup.new_tag("title")
-title_tag.string = "Static HTML/CSS File Tree"
-
-link_css = soup.new_tag("link", rel="stylesheet", href="style.css")
-
-# 5. Assembler le <head> en y ajoutant les balises créées
-head_tag.append(meta_charset)
-
-head_tag.append(title_tag)
-head_tag.append(link_css)
-
-# 6. Insérer le <head> tout au début du document (ou au début de la balise <html>)
-if soup.html:
-    soup.html.insert(0, head_tag)
-else:
-    # Si la balise <html> n'existe pas du tout, on l'ajoute au début du fichier
-    soup.insert(0, head_tag)
-
-
-while True:
-    element = soup.find(string=re.compile(SHORTCUT))
-    if element is None:
-        break
-    parameters = element[1+len(SHORTCUT):]
-    path,depth = parameters.split(' ')
-    print(path,depth)
-    try:
-        print(f'\npath : {path}')
-        print(f'depth : {depth}\n')
-        try : 
-            depth = int(depth)
-        except:
-            depth = 1
-        tree = build_tree(path,depth)
-        print(tree)
-        html = render_page(tree)
-        
-    except:
-        html = ''
-        pass
-    nouveau_html = BeautifulSoup(html, "html.parser")
+final_html = ""
+last_index = 0
+for index in indexs: # Add everything to the html until the last command
     
-    balise_parente = element.parent
-    balise_parente.clear()
-    balise_parente.append(nouveau_html)
+    parameters = CommonMark_content[index][1 + len(SHORTCUT):]
+    path, depth = parameters.strip().split(' ')
+    
+    
+    tree = build_tree(path, depth)
+    
+    
+    markdown_chunk = "\n".join(CommonMark_content[last_index:index])
+    final_html += mistletoe.markdown(markdown_chunk)
+    final_html += render_page(tree)
+    
+    
+    last_index = index + 1 
 
-#add stuff
-with open("output.html", "w", encoding="utf-8") as f:
-    f.write(soup.prettify())
+if indexs: # Add the remaining chunk of text to the html
+    remaining_chunk = "\n".join(CommonMark_content[last_index:])
+    final_html += mistletoe.markdown(remaining_chunk)
 
+else: ## If no command
+    final_html += mistletoe.markdown("\n".join(CommonMark_content))
+
+
+OUTPUT_FILE = "output.html"
+with open(OUTPUT_FILE, "w", encoding="utf-8") as fout:
+    fout.write(final_html)
 print("Le head a été ajouté avec succès dans 'index.html' !")
+
+
